ed4b4571c22534cb7ac46a8a4c6cf030f8d0f4f7
Shivansh Singh <2310shivansh@gmail.com>
2026-09-13 22:48:34 +0530

    docs: add Apache 2.0 release notice to Lean module headers

--- SubmodularGreedy/CheckAll.lean
+++ SubmodularGreedy/CheckAll.lean
@@ -1,5 +1,6 @@
 /-
 Copyright (c) 2026 Shivansh Singh. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Shivansh Singh
 -/
 import SubmodularGreedy.ClosureSystem
